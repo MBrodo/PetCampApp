853f09502c89819dce2edc652696c7de5331c41f
Aleksandr <aleqqq132@gmail.com>
2021-10-29 16:56:54 +0300

    create: fourth screen(payments)

--- src/components/book/BookProcess/Payments/PaymentContainer.js
+++ src/components/book/BookProcess/Payments/PaymentContainer.js
@@ -1,7 +1,11 @@
 import React from 'react'
-
 import { PaymentView } from './PaymentView'
+import { useNavigation } from '@react-navigation/native'
 
 export const PaymentContainer = () => {
-	return <PaymentView />
+	const navigation = useNavigation()
+	const thirdStep = () => {
+		navigation.navigate('Congrats')
+	}
+	return <PaymentView thirdStep={thirdStep} />
 }
