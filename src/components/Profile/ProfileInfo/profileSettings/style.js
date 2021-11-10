b252586931e2851a95cb645d7beb9a13b98dda8e
Aleksandr <ger5051@gmail.com>
2021-11-10 12:34:56 +0300

    fix: settings layout

--- src/components/Profile/ProfileInfo/profileSettings/style.js
+++ src/components/Profile/ProfileInfo/profileSettings/style.js
@@ -1,10 +1,10 @@
 import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
-	wrapper: {
+	mainContainer: {
 		backgroundColor: 'rgba(60, 176, 157, 0.1)',
-		width: '100%',
-		height: 580,
+	},
+	wrapper: {
 		alignItems: 'center',
 	},
 	imageBackGround: {
