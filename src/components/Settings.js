9e9476876e940ff24305db789c0f6d2d0fff85eb
Aleksandr <ger5051@gmail.com>
2021-11-22 16:49:45 +0300

    fix: settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MySettingsContainer } from './Settings/MySettingsConainer'
 import { SettingsUnLoggedContainer } from './Settings/SettingsUnLoggedContainer/SettingsUnLoggedContainer'
 import { useSelector } from 'react-redux'
 
 export const Settings = () => {
-	const authenticate = useSelector((state) => state.auth.status)
-
-	return <>{authenticate ? <MySettingsContainer /> : <SettingsUnLoggedContainer />}</>
+	const [authenticate] = useState(useSelector((state) => state.auth.status))
+	const userID = useSelector((state) => state.user.id.length)
+	return (userID === 0 ? authenticate : true) ? (
+		<MySettingsContainer />
+	) : (
+		<SettingsUnLoggedContainer />
+	)
 }
