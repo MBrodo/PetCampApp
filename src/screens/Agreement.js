2d82687985d33f4f1db55ff859909189253d184b
Yahor Vasileuski <Yahor.Vasileuski@itechart-group.com>
2021-11-29 16:26:13 +0300

    init: add Agreement

---
+++ src/screens/Agreement.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+import { styles } from './styles'
+
+export const Agreement = () => {
+	return (
+		<ScrollView style={styles.background}>
+			<View style={styles.agreementMain}>
+				<Text style={styles.agreementTitle}>Client Agreement</Text>
+				<Text>
+					1.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse tortor tortor erat
+					quis tortor fermentum turpis arcu aliquet. Massa non convallis id placerat elementum velit
+					convallis dis. Ultrices cras quis ullamcorper faucibus arcu arcu commodo, pellentesque.
+					Dui, urna ultrices gravida egestas orci. Auctor mauris nisl morbi bibendum magna turpis
+					porttitor nullam imperdiet. Viverra aliquet neque, id nec amet morbi eros, velit. Mollis
+					quisque consectetur odio malesuada fermentum tincidunt faucibus vestibulum, nunc. Cursus
+					ut integer ornare libero. Erat facilisis euismod adipiscing nullam sit. Phasellus nunc, ut
+					odio urna, varius cursus.
+				</Text>
+				<Text>
+					2.Amet, vel erat tempus duis enim. Orci, tincidunt est aenean felis. Senectus integer
+					porttitor odio integer bibendum. Nisl, arcu habitasse sollicitudin quis at ut felis
+					tempor. In feugiat nunc, erat tincidunt sollicitudin vitae, commodo. Nec volutpat
+					venenatis sit tempor, dictumst justo faucibus euismod. Facilisis auctor convallis augue
+					eget. Duis nam sit morbi et. Amet aenean arcu mauris lacus.
+				</Text>
+				<Text>
+					3.Sit elit scelerisque gravida cursus felis tellus. Arcu mi at id pharetra, orci
+					consectetur. Vitae enim adipiscing netus cras mauris morbi metus quis natoque. Elementum,
+					rhoncus volutpat condimentum nunc sapien pharetra quis. Sed elementum vulputate justo
+					facilisi euismod. Amet, pellentesque nec mollis turpis sem orci adipiscing. Felis amet
+					amet sed quisque. Cras quis a vivamus morbi nibh id. Dui fringilla viverra lectus dolor
+					erat vel tellus vel. Varius aenean pellentesque bibendum in tellus. Tellus, ultricies
+					suspendisse nunc urna, interdum quam phasellus faucibus ut. Amet viverra cursus senectus
+					nunc urna volutpat, nibh. Ultrices mi malesuada nulla id leo varius. Sed lobortis risus,
+					enim elementum duis pharetra, at et a.
+				</Text>
+				<Text>
+					4.Quisque aenean imperdiet tristique in morbi ac malesuada bibendum. Nisi, phasellus
+					scelerisque nam aliquam scelerisque senectus. Vivamus hendrerit ac nunc, quis sed ornare.
+					Eu porttitor sed at sed pulvinar est quam rhoncus. Sapien sagittis molestie non quis
+					lectus.
+				</Text>
+				<Text>
+					5.Mauris in placerat aliquam risus aliquam est pharetra sem nisl. Ullamcorper placerat
+					nibh ut vitae, tincidunt velit. Sem ultrices vitae, eget faucibus ultricies volutpat amet
+					sem sit. Ut ornare diam at eget nec. Sit nulla arcu nulla purus nibh eros, sed at id.
+					Semper morbi ut nisl accumsan aenean id a. A, tortor, rhoncus sed nam enim. At nulla arcu,
+					elit, eget. Sit placerat enim ultricies sit. Eget tincidunt curabitur consequat amet amet
+					nisi ut velit, tortor. Justo sit tempus rhoncus at feugiat pulvinar cras neque. Dictum
+					neque lorem amet volutpat, fames augue. Sem in pulvinar feugiat dignissim ipsum nibh
+					varius. Ut nunc porttitor at volutpat fringilla.
+				</Text>
+				<Text>
+					6.Nisi, massa viverra scelerisque dolor at. Venenatis pellentesque sed duis non tincidunt
+					erat. Libero neque eget volutpat sit fermentum pellentesque. Elit, purus lacus, quis
+					tortor nunc aliquet eu. Viverra pharetra nec malesuada fermentum faucibus dignissim semper
+					vitae interdum.{' '}
+				</Text>
+				<Text>
+					{' '}
+					7.Condimentum mauris odio amet mauris consectetur pellentesque. Faucibus phasellus est
+					magna volutpat lectus. Viverra vitae scelerisque sem interdum. Aliquet urna consequat urna
+					aliquet phasellus. Arcu fames id et enim ut. Lorem nisi eget tellus, tellus interdum
+					dictum. Pellentesque id tellus mattis nulla nunc morbi malesuada at semper. Vel egestas
+					facilisi sit fermentum faucibus.
+				</Text>
+			</View>
+		</ScrollView>
+	)
+}
